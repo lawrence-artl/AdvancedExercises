108e080dac5cffd3a9980323c274f40b07db4de9
Lawrence Artl <lawrence.artl@gmail.com>
2020-10-11 20:06:44 -0700

    Add project files.

---
+++ Exercise01/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace Exercise01
+{
+    public class Employee
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }       //ensure value is listed second
+
+        }
+        private int age;
+        
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        private bool directDeposit;
+        public bool DirectDeposit
+        {
+            get { return directDeposit; }
+            set { directDeposit = value; }
+        }
+
+
+    }
+}
---
+++ Exercise01/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;   //for Lists
+
+namespace Exercise01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a name for an employee:");
+            string newName = Console.ReadLine();
+            Console.WriteLine("Enter the employee's age:");
+            string Agestr = Console.ReadLine();
+            Console.WriteLine("Did the employee apply for Direct Deposit?");
+            string DirectD = Console.ReadLine();
+
+            int AgeNum = Int32.Parse(Agestr);
+            bool dd;
+            DirectD.ToLower();
+            if (DirectD.Equals("yes") || DirectD.Equals("y"))
+            {
+                dd = true;
+            }
+            else
+                dd = false;
+            Console.WriteLine(newName);
+
+            List <Employee> employee = new List <Employee>();
+
+            employee.Add(new Employee() { Name = newName, Age = AgeNum, DirectDeposit = dd });
+
+            /*
+            employee.Name = newName;
+            employee.Age = AgeNum;
+            employee.DirectDeposit = dd;
+            */
+
+            Console.WriteLine("\nThe Employee entered is:");
+            Console.WriteLine("The employee's name is: " + employee[0].Name);   
+            Console.WriteLine("The employee's age is " + employee[0].Age);
+            Console.WriteLine("The employee has signed up for direct deposit? " + employee[0].DirectDeposit);
+
+
+
+
+
+        }
+    }
+}
