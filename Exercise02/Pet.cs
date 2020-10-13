d726f883527b2d5514b8463b850f20a257713814
Lawrence Artl <lawrence.artl@gmail.com>
2020-10-12 20:35:23 -0700

    Added exercise02

---
+++ Exercise02/Pet.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise02
+{
+    class Pet
+    {
+        public static int count = 0;
+        public Pet()
+        {
+            count++;
+        }
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private int age;
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+
+        }
+        private int num;
+        public int Num
+        {
+            get { return num; }
+            set { num = count; }
+
+        }
+
+
+
+    }
+}
---
+++ Exercise02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pet cat = new Pet();
+            Pet dog = new Pet();
+
+            cat.Name = "Smoke";
+            cat.Age = 13;
+
+            dog.Name = "Barky";
+            dog.Age = 10;
+
+            Console.WriteLine("There are " + Pet.count + " pets here.");
+
+            Pet hamster = new Pet();
+            hamster.Name = "Herman";
+            hamster.Age = 5;
+            hamster.Num = Pet.count;
+
+            Console.WriteLine("There are " + Pet.count + " pets here now.");
+
+            Console.WriteLine("The cat's name is " + cat.Name);
+            Console.WriteLine("The pet at position " + hamster.Num + " is named " + hamster.Name);
+            
+
+        
+        }
+    }
+}
