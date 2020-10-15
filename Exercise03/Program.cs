7411127fad47ac950b094d7d54e4bce2c8eaee30
Lawrence Artl <lawrence.artl@gmail.com>
2020-10-14 19:06:51 -0700

    Exercise 3 Lists

---
+++ Exercise03/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Tool> tools = new List<Tool>();
+
+            tools.Add(new Tool() { Name = "hammer", ToolID = 1334 });
+            tools.Add(new Tool() { Name = "screwdriver", ToolID = 4355 });
+            tools.Add(new Tool() { Name = "wrench", ToolID = 5344 });
+
+            Console.WriteLine();
+            foreach (Tool listTool in tools)
+            {
+                Console.WriteLine(listTool);
+            }
+
+
+
+
+            /*
+            Console.WriteLine();
+            foreach (Part aPart in parts)
+            {
+                Console.WriteLine(aPart);
+            }
+            */
+
+
+
+
+        }
+    }
+}
---
+++ Exercise03/Tool.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise03
+{
+    class Tool
+    {
+        public Tool()
+        {
+            count++;
+        }
+        public override string ToString()
+        {
+            return "ID: " + toolID + "    Tool: " + name;
+        }
+
+        private string name;
+        private int count;
+        private double price;
+        private int toolID;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Count
+        {
+            get { return count; }
+            set { count = value; }
+        }
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        public int ToolID
+        {
+            get { return toolID; }
+            set { toolID = value; }
+        }
+    }
+}
