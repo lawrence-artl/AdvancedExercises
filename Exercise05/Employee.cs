8f3b2606517fe6fc1c3c9e7b37ef9206022072ef
Lawrence Artl <lawrence.artl@gmail.com>
2020-10-17 18:39:22 -0700

    formatting lists and printing, adding items to list

---
+++ Exercise05/Employee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise05
+{
+    class Employee
+    {
+        private string name;
+        private int age;
+        private double hourly;
+
+        private static int count = 0;
+
+        public Employee()
+        {
+            count++;
+        }
+        public static int Count
+        {
+            get { return count; }
+        }
+
+        public string Name
+        
+            { get; set; }
+        
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public double Hourly
+        {
+            get { return hourly; }
+            set { hourly = value; }
+        }
+
+        public static void PrintList(List<Employee> employees)  //needs static keyword to work with program.cs
+        {
+            Console.WriteLine("{0,-20} {1,5} {2, 11}", "Name", "Hourly", "Age");
+            for (int i = 0; i <= count - 1; i++)
+            {
+                string newName = employees[i].Name;
+                double newHourly = employees[i].Hourly;
+                int newAge = employees[i].Age;
+                
+                
+                Console.WriteLine("{0,-20} {1,1} {2,5:N2} {3,10}", newName, "$", newHourly, newAge);
+            }
+        }
+    }
+}
---
+++ Exercise05/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+
+            employees.Add(new Employee() { Name = "John", Age = 33, Hourly = 11.50 });
+            employees.Add(new Employee() { Name = "Angelina", Age = 42, Hourly = 12.30 });
+            employees.Add(new Employee() { Name = "Sofia", Age = 16, Hourly = 11.00 });
+            employees.Add(new Employee() { Name = "Trinity", Age = 17, Hourly = 11.00 });
+
+            //Employee.PrintList(employees);
+
+            employees.Add(new Employee() { Name = "Lawrence", Age = 37, Hourly = 22.00 });
+
+
+            Employee.PrintList(employees);
+
+            bool inMenu = true;
+            do
+            {
+                Console.WriteLine("Add a new employee? y/n");
+                string selection = Console.ReadLine();
+                if (selection.Equals("n"))
+                    inMenu = false;
+                else if (selection.Equals("print"))
+                    Employee.PrintList(employees);
+                else
+                {
+                    Console.WriteLine("Enter a name: ");
+                    string newName = Console.ReadLine();
+                    Console.WriteLine("Enter their age: ");
+                    string strAge = Console.ReadLine();
+                    int newAge = Int32.Parse(strAge);
+                    Console.WriteLine("Enter their hourly rate: ");
+                    string strHourly = Console.ReadLine();
+                    double newHourly = Double.Parse(strHourly);
+
+                    employees.Add(new Employee() { Name = newName, Age = newAge, Hourly = newHourly });
+
+                    Console.WriteLine("Employee " + employees[Employee.Count - 1].Name + " added.");
+                }
+            } while (inMenu);
+
+            Employee.PrintList(employees);
+
+        }
+        
+    }
+}
