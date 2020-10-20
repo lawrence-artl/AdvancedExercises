bcddbe0c852ab60416bfb691139df1ac5afec337
Lawrence Artl <lawrence.artl@gmail.com>
2020-10-19 19:36:55 -0700

    exercise 7, candies; menu with help, search, sort, remove from list

---
+++ Exercise07/Candy.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise07
+{
+    class Candy
+    {
+        private string name;
+        private double price;
+        public static int count;
+
+        public Candy()
+        {
+            count++;
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        public static void PrintList(List<Candy> candies)
+        {
+            Console.WriteLine("{0,-20} {1,7}", "Candy", "Price");
+            Console.WriteLine(new string('-', 28));
+            for (int i = 0; i <= count - 1; i++)
+            {
+                Console.WriteLine("{0,-20} {1,1} {2,5:N2}", candies[i].Name, "$", candies[i].Price);
+            }
+
+        }
+        public static void Add(List<Candy> candies)
+        {
+            string newName;
+            string strPrice;
+            double newPrice;
+            Console.Write("Enter a name for the candy: ");
+            newName = Console.ReadLine();
+            Console.Write("Enter the price for " + newName + ": ");
+            strPrice = Console.ReadLine();
+            newPrice = Double.Parse(strPrice);
+            candies.Add(new Candy() { Name = newName, Price = newPrice });
+
+        }
+        public static void Search(List<Candy> candies)
+        {
+
+        }
+        public static void Sort(List<Candy> candies)
+        {
+
+        }
+        public static void Remove(List<Candy> candies)
+        {
+
+        }
+    }
+}
---
+++ Exercise07/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Candy> candies = new List<Candy>();
+
+            candies.Add(new Candy() { Name = "Snickers", Price = 4.00 });
+            candies.Add(new Candy() { Name = "Skittles", Price = 5.50 });
+
+            Candy.PrintList(candies);
+
+            bool inMenu = true;
+            do
+            {
+                Console.WriteLine("Type your selection");
+                string selection = Console.ReadLine();
+                selection.ToLower();
+                selection.Trim();
+                if (selection.Equals("print"))
+                {
+                    Candy.PrintList(candies);
+                }
+                else if (selection.Equals("add"))
+                {
+                    Candy.Add(candies);
+                    bool addMore = true;
+                    do
+                    {
+                        Console.Write("Would you like to add another? yes/no ");
+                        string newSelection = Console.ReadLine();
+                        newSelection.ToLower();
+                        newSelection.Trim();
+                        newSelection.Substring(0, 1);
+                        if (newSelection.StartsWith("n"))
+                        {
+                            addMore = false;
+                        }
+                        else if (newSelection.StartsWith("y"))
+                        {
+                            Candy.Add(candies);
+                        }
+                        else
+                        {
+                            Console.WriteLine("That is not a valid selection; type 'yes' or 'no' ");
+                        }
+                    } while (addMore);
+                }
+                else if (selection.Equals("exit"))
+                {
+                    inMenu = false;
+                }
+                else if (selection.Equals("help"))
+                {
+                    Refresh.refreshPage();
+                    Console.WriteLine("Menu Options:\n'Add'\n'Print'\n'Help'\n'Clear'\n'Search'\n'Remove'\n'Exit'\n\n" +
+                        "Press any key to clear and continue.");
+                    Console.ReadKey();
+                    Refresh.refreshPage();
+                }
+                else if (selection.Equals("clear"))
+                {
+                    Refresh.refreshPage();
+                }
+                else
+                {
+                    Console.WriteLine("That was not a valid selection, please try again.");
+                }
+                
+            } while (inMenu);
+        }
+    }
+}
